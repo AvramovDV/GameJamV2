28e7700ed3097c6721249f7a7421e8a52189e015
Denis Avramov <avramov.91@mail.ru>
2021-12-05 13:05:55 +0300

    weapon images

--- GameJamProject/Assets/GameJamAvramov/Scripts/Views/ZombieImageView.cs
+++ GameJamProject/Assets/GameJamAvramov/Scripts/Views/ZombieImageView.cs
@@ -6,12 +6,21 @@ namespace GameJam
     public class ZombieImageView : MonoBehaviour
     {
         [SerializeField] private Image _image;
+        [SerializeField] private Image _weaponImage;
         [SerializeField] private Color _selectedColor;
         [SerializeField] private Color _deselectedColor;
 
-        public void Select() => _image.color = _selectedColor;
+        public void Select()
+        {
+            _image.color = _selectedColor;
+            _weaponImage.color = _selectedColor;
+        }
 
-        public void Deselect() => _image.color = _deselectedColor;
+        public void Deselect()
+        {
+            _image.color = _deselectedColor;
+            _weaponImage.color = _deselectedColor;
+        }
     }
 }
 
